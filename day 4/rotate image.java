702ee16af96e951e817f6156c11b3c853e9874c6
20CC027-Roshne <110483606+20CC027-Roshne@users.noreply.github.com>
2022-12-21 20:14:51 +0530

    Create rotate image.java

---
+++ day 4/rotate image.java
@@ -0,0 +1,28 @@
+public void rotate(int[][] matrix) {
+for(int i=0,p=matrix.length-1;i<matrix.length/2;i++)
+{
+for(int j=0;j<matrix[0].length;j++)
+{
+int temp=0;
+temp=matrix[i][j];
+matrix[i][j]=matrix[p][j];
+matrix[p][j]=temp;
+}
+p--;
+}
+
+    for(int i=0;i<matrix.length;i++)
+    {
+        for(int j=i;j<matrix[0].length;j++)
+        {
+            
+            
+            int temp=0;
+            temp=matrix[i][j];
+            matrix[i][j]=matrix[j][i];
+            matrix[j][i]=temp;    
+            
+            
+        }
+    }
+}
